9e2ad98b6da26e859b9f8b64a1021193e3ae8da9
Alex <rubanovv.alex@gmail.com>
2023-03-29 00:49:46 +0200

    added ErrorPage for invalid routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Posts from './pages/Posts';
+import ErrorPage from './pages/ErrorPage';
 import './styles/index.css'
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/posts',
-//     element: <Posts/>
-//   },
-//   {
-//     path: '/about',
-//     element: <About/>
-//   },
-//   {
-//     path: '/',
-//     element: <Navbar/>,
-//     errorElement: <ErrorPage/>
-//   }
-// ]);
 
 function App() {
   
   return (
     <div className="App">
       <BrowserRouter > 
+        <Navbar/>
+        
+        <Switch>
+          <Route exact path="/about">
+            <About/>  
+          </Route> 
 
-        <Route path="/">
-          <Navbar/>
-        </Route>
+          <Route exact path="/posts">
+            <Posts/>
+          </Route>
 
-        <Route path="/about">
-          <About/>  
-        </Route> 
+          <Route path="/404">
+            <ErrorPage/>
+          </Route>
 
-        <Route path="/posts">
-          <Posts/>
-        </Route>
-
+          <Redirect to="/404"/>
+        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div className="navbar">
         <div className="navbar__links">
-            <Link to={'/about'}>About</Link>
-            <Link to={'/posts'}>Posts</Link>
+            <Link to="/about">About</Link>
+            <Link to="/posts">Posts</Link>
         </div>
     </div>
   )
