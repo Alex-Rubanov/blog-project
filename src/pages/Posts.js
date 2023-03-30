7134e1acf81f53f7ff32a839c1eb46e64a75f64a
Alex <rubanovv.alex@gmail.com>
2023-03-31 00:39:27 +0200

    added infinite scroll for posts as an alternative for page pagination and also added an option to select number of posts you can add dynamically by scrolling

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -13,6 +13,9 @@ import MyModal from '../components/UI/myModal/MyModal';
 import MyButton from '../components/UI/button/MyButton';
 
 import '../styles/index.css'
+import { useRef } from 'react';
+import { useObserver } from '../hooks/useObserver';
+import MySelect from '../components/UI/select/MySelect';
 
 
 const Posts = () => {
@@ -22,25 +25,28 @@ const Posts = () => {
 
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-
-
+  const [limit, setLimit] = useState(10);
+  const lastElement = useRef();
 
   const searchedAndSortedPosts = usePosts(posts, filter.sort, filter.query);
   const [fetchPosts, isLoading, postError] = useFetching(async () => {
-    const limit = 10;
 
     const response = await PostServices.getAll(limit, currentPage);
     const totalPostsCount = response.headers['x-total-count'];
     const totalNumberOfPages = getTotalPages(totalPostsCount, limit);
 
     setTotalPages(totalNumberOfPages);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
+  });
+
+  useObserver(lastElement, currentPage < totalPages, isLoading, () => {
+    setCurrentPage(currentPage + 1);
   });
 
   useEffect(() => {
     fetchPosts();
     // eslint-disable-next-line
-  }, [currentPage])
+  }, [currentPage, limit])
  
   const addPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -63,10 +69,21 @@ const Posts = () => {
         filter={filter}
         setFilter={setFilter}
       />
+      <MySelect
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue="Number of posts on a page"
+        options={[
+          {value: 5, name: '5'},
+          {value: 10, name: '10'},
+          {value: 25, name: '25'},
+          {value: -1, name: 'Show all'}
+        ]}
+      />
 
       {postError && <h2 style={{textAlign: 'center', marginTop: '30px'}}>Ooops! Error occured during request. {postError}</h2>}
-      {isLoading
-        ? <SpinnerCircular 
+      { isLoading &&
+         <SpinnerCircular 
             style={{ display: 'block', margin: '50px auto'}} 
             size={69} 
             thickness={100} 
@@ -74,19 +91,20 @@ const Posts = () => {
             color="teal" 
             secondaryColor="rgba(0, 0, 0, 0.44)" 
           />
-      : <PostList 
-          posts={searchedAndSortedPosts} 
-          title="List of available posts"
-          deletePost={deletePost}
-        />
       }
+      <PostList 
+        posts={searchedAndSortedPosts} 
+        title="List of available posts"
+        deletePost={deletePost}
+      />
+      <div style={{height: 20}} ref={lastElement}></div>
 
-      <Pagination
+      {/* <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
         isLoading={isLoading}
-      />      
+      />       */}
     </div>
   );
 }
